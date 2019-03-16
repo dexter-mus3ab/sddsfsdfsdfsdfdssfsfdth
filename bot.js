a3e7c28c9b41732ff4fb1cc15398b9aa09d009f2
dexter-mus3ab <47899367+dexter-mus3ab@users.noreply.github.com>
2019-03-16 08:36:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client()
  
 client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "514131726868545556" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "514126485230190665" && ch.type === 'voice').join();
 });
 
 client.login(process.env.BOT_TOKEN);
